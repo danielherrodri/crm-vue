e42f1df23bd2931f0aca3f18d65ede68e9d894a7
drodriguez <drodriguez@izzi.mx>
2024-07-05 11:43:46 -0600

    Vídeo 187 - Creando la vista y ruta para nuevos clientes.

---
+++ src/views/NuevoClienteView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+<template>
+    <div>
+        <div>
+            <RouterLink :to="{ name: 'inicio' }">Ir a inicio</RouterLink>
+        </div>
+        <h1 class="text-4xl font-extrabold text-slate-500">Agregar Cliente</h1>
+    </div>
+</template>
