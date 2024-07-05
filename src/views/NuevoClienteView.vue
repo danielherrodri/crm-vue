1c4a206f1846516596c8d027d73a35871ece2bd4
drodriguez <drodriguez@izzi.mx>
2024-07-05 12:50:02 -0600

    Vídeo 195 - Agregando campos al formulario y más reglas de validación de FormKit

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -13,9 +13,17 @@ import { FormKit } from '@formkit/vue'
         <div class="mx-auto mt-10 bg-white shadow">
             <div class="mx-auto md:w-2/3 py-20 px-6">
                 <FormKit type="form">
-                    <FormKit type="text" label="Nombre" placeholder="Nombre del cliente"
-                        help="Coloca el nombre del cliente que deseas registrar" validation="required"
+                    <FormKit type="text" label="Nombre" placeholder="Nombre del cliente" validation="required"
                         :validation-messages="{ required: 'El nombre del cliente es obligatorio' }" />
+
+                    <FormKit type="text" label="Apellido" placeholder="Apellido del cliente" validation="required"
+                        :validation-messages="{ required: 'El apellido del cliente es obligatorio' }" />
+
+                    <FormKit type="email" label="Email" placeholder="Email del cliente" validation="required|email"
+                        :validation-messages="{ required: 'El email del cliente es obligatorio', email: 'Coloca un email válido' }" />
+
+                    <FormKit type="text" label="Teléfono" placeholder="Teléfono XXX-XXX-XXX" validation="required"
+                        :validation-messages="{ required: 'El teléfono del cliente es obligatorio' }" />
                 </FormKit>
             </div>
         </div>
