52787cbc76b6dff2cd918a5c5feb5da32cf6e728
drodriguez <drodriguez@izzi.mx>
2024-07-05 12:05:37 -0600

    Vídeo 189 - Mostrandoel nombre de la página actual.

---
+++ src/components/UI/Heading.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1 class="text-4xl font-extrabold text-slate-900">
+        <slot></slot>
+    </h1>
+</template>
--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -1,11 +1,12 @@
 <script setup>
 import RouterLink from '../components/UI/RouterLink.vue'
+import Heading from '../components/UI/Heading.vue'
 </script>
 <template>
     <div>
         <div class="flex justify-end">
             <RouterLink to="inicio">Ir a inicio</RouterLink>
         </div>
-        <h1 class="text-4xl font-extrabold text-slate-500">Agregar Cliente</h1>
+        <Heading>Agregar Cliente</Heading>
     </div>
 </template>
