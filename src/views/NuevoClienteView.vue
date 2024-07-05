405eb81a4152ccc86b5735d95737e4118df5307f
drodriguez <drodriguez@izzi.mx>
2024-07-05 12:29:55 -0600

    Vídeo 193 - Agregando el primer campo del formulario y que incluye FormKit

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -12,7 +12,9 @@ import { FormKit } from '@formkit/vue'
 
         <div class="mx-auto mt-10 bg-white shadow">
             <FormKit type="form">
-
+                <FormKit type="text" label="Nombre" placeholder="Nombre del cliente"
+                    help="Coloca el nombre del cliente que deseas registrar" validation="required"
+                    :validation-messages="{ required: 'El nombre del cliente es obligatorio' }" />
             </FormKit>
         </div>
     </div>
