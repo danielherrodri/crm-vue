99eda58e5ad2d67c8c0c9cc2afb900068af3e948
danielherrodri <84465666+danielherrodri@users.noreply.github.com>
2024-07-05 18:54:11 -0600

    Vídeo 204 - Creando un computed property que revise si tenemos clientes.

--- src/views/ClientesView.vue
+++ src/views/ClientesView.vue
@@ -1,28 +1,53 @@
 <script setup>
-import { onMounted, ref } from "vue";
+import { onMounted, ref, computed } from "vue";
 import RouterLink from "../components/UI/RouterLink.vue";
 import Heading from "../components/UI/Heading.vue";
 import axios from "axios";
 
 const clientes = ref([]);
 onMounted(() => {
-  axios("http://localhost:3000/clientes")
-    .then(({ data }) => (clientes.value = data))
-    .catch((error) => console.log(error));
+    axios("http://localhost:3000/clientes")
+        .then(({ data }) => (clientes.value = data))
+        .catch((error) => console.log(error));
 });
-
-const 
+const existenClientes = computed(() => {
+    return clientes.value.length > 0;
+});
 </script>
 <template>
-  <div>
-    <div class="flex justify-end">
-      <RouterLink
-        class="bloc md:inline-block rounded bg-blue-600 py-2 px-3"
-        to="agregar-cliente"
-      >
-        Agregar Cliente
-      </RouterLink>
+    <div>
+        <div class="flex justify-end">
+            <RouterLink class="bloc md:inline-block rounded bg-blue-600 py-2 px-3" to="agregar-cliente">
+                Agregar Cliente
+            </RouterLink>
+        </div>
+        <Heading>Listado de Clientes</Heading>
+
+        <div v-if="existenClientes" class="flow-root mx-auto mt-10 p-5 bg-white shadow">
+            <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div class="min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                    <table class="min-w-full divide-y divide-gray-300">
+                        <thead>
+                            <tr>
+                                <th scope="col" class="p-2 text-left text-sm font-extrabold text-gray-600">
+                                    Nombre
+                                </th>
+                                <th scope="col" class="p-2 text-left text-sm font-extrabold text-gray-600">
+                                    Empresa
+                                </th>
+                                <th scope="col" class="p-2 text-left text-sm font-extrabold text-gray-600">
+                                    Estado
+                                </th>
+                                <th scope="col" class="p-2 text-left text-sm font-extrabold text-gray-600">
+                                    Acciones
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody class="divide-y divide-gray-200 bg-white"></tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <p v-else class="text-center mt-10">No hay clientes</p>
     </div>
-    <Heading>Listado de Clientes</Heading>
-  </div>
 </template>
