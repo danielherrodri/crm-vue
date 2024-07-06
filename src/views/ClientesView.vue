74e67e725a3640f6ecb759a9bbe3bfe0e05b46b8
danielherrodri <84465666+danielherrodri@users.noreply.github.com>
2024-07-05 18:40:52 -0600

    Vídeo 202 - Consulando la REST API en Vue.js

--- src/views/ClientesView.vue
+++ src/views/ClientesView.vue
@@ -1,14 +1,28 @@
 <script setup>
-import RouterLink from '../components/UI/RouterLink.vue'
-import Heading from '../components/UI/Heading.vue'
+import { onMounted } from "vue";
+import RouterLink from "../components/UI/RouterLink.vue";
+import Heading from "../components/UI/Heading.vue";
+import axios from "axios";
+
+onMounted(() => {
+  axios
+    .get("http://localhost:3000/clientes")
+    .then(({ data }) => {
+      console.log(data);
+    })
+    .catch((error) => console.log(error));
+});
 </script>
 <template>
-    <div>
-        <div class="flex justify-end">
-            <RouterLink class="bloc md:inline-block rounded bg-blue-600 py-2 px-3" to="agregar-cliente">
-                Agregar Cliente
-            </RouterLink>
-        </div>
-        <Heading>Listado de Clientes</Heading>
+  <div>
+    <div class="flex justify-end">
+      <RouterLink
+        class="bloc md:inline-block rounded bg-blue-600 py-2 px-3"
+        to="agregar-cliente"
+      >
+        Agregar Cliente
+      </RouterLink>
     </div>
+    <Heading>Listado de Clientes</Heading>
+  </div>
 </template>
